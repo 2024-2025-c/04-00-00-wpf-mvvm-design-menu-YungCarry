78dc29daa40996f940b7dbd1752183b14ef2e1d3
Kéri Dominik <81580900+YungCarry@users.noreply.github.com>
2024-11-27 10:54:34 +0100

    Delete KretaBasicSchoolSystem.Desktop directory

--- KretaBasicSchoolSystem.Desktop/ViewModels/Base/BaseViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace KretaBasicSchoolSystem.Desktop.ViewModels.Base
-{
-    public  abstract class BaseViewModel : ObservableObject
-    {
-    }
-}
